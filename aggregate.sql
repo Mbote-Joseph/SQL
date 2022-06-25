7d30d4f23cb0624deef2a7a3c8225728d5d2a125
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-06-25 23:47:57 +0300

    Aggregate Function SQL

---
+++ aggregate.sql
@@ -0,0 +1,25 @@
+-- Column references
+SELECT COUNT(*) AS 'total_movies', rating
+FROM movies
+GROUP BY 2 
+ORDER BY 1;
+-- GROUP BY 2 is equivalent to GROUP BY rating
+-- ORDER BY 1 is equivalent to ORDER BY total_movies
+
+-- MAX() and MIN() aggregate functions
+SELECT MAX(rating) AS 'highest_rated_movie'
+FROM movies;
+
+SELECT MIN(rating) AS 'lowest_rated_movie'
+FROM movies;
+
+-- SUM and AVG aggregate functions
+SELECT SUM(rating) AS 'total_rating'
+FROM movies;
+
+SELECT AVG(rating) AS 'average_rating'
+FROM movies;
+
+-- COUNT aggregate function
+SELECT COUNT(*) AS 'total_movies'
+FROM movies;
