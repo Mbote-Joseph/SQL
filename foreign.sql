a38c7542c7b044dbe626c30c55af88337cfdfb67
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-06-26 20:48:05 +0300

    New Branch

--- foreign.sql
+++ foreign.sql
@@ -4,3 +4,9 @@ FROM books
 JOIN authors
 ON books.author_id = authors.id
 WHERE authors.name = 'John Doe';
+
+SELECT COUNT(*) AS count, authors.name
+FROM books
+JOIN authors.name
+ON books.author_id = authors.id
+WHERE authors.name = 'John Doe';
